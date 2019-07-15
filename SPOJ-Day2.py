d6c10e34a753b0e176ba987bace3d8f96c25296b
Janek <47718562+Haans001@users.noreply.github.com>
2019-07-15 13:25:40 +0200

    Update SPOJ-Day2.py

--- SPOJ-Day2.py
+++ SPOJ-Day2.py
@@ -91,3 +91,46 @@ print(result)
 
 
 
+
+
+# JSZYFR2 - Szyfrowanie2
+
+import sys
+chainLength = int(input("chainLength: "))
+chains = input("chains: ").split();
+chains = [int(chain) for chain in chains]
+
+def is_prime(n):
+     if n <= 3:
+        return n > 1
+     elif n%2 == 0 or n%3 == 0:
+        return False
+     i = 5
+     while i * i <= n:
+        if n%i == 0 or n%(i + 2) == 0:
+            return False
+        i=i + 6
+     return True
+
+def getValidNumber():   
+  for i in range(120,151):
+    valid = True
+    for num in chains:
+      current = num%i
+      if not (current >= 65 and current <=90):
+        valid = False
+        break;
+
+    if valid and is_prime(i):
+      return i;
+
+if len(chains) < chainLength:
+  print("invalid input")
+  sys.exit()
+
+result = ''
+num = getValidNumber()
+for chain in chains:
+  result+=chr(chain%num)
+print(result)
+
