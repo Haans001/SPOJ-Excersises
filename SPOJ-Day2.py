4f8e864ecd4275381fc953a9142fef8568ea8550
Janek <47718562+Haans001@users.noreply.github.com>
2019-07-15 11:47:45 +0200

    Update SPOJ-Day2.py

--- SPOJ-Day2.py
+++ SPOJ-Day2.py
@@ -21,3 +21,25 @@ sortedList = sorted(namesDict.items(), key=operator.itemgetter(1), reverse=True)
 
 for el in sortedList:
   print(f"{el[0]} {el[1]}")
+
+
+
+
+# KMP - wyszukiwanie wzorca w tekscie
+patternLen = int(input("Enter a pattern length: "))
+pattern = input("Enter a pattern: ")
+text = input("Enter a text: ")
+
+indexes = []
+
+for i in range(len(text)- patternLen +1):
+  patternIndex = 0
+  isValid = True
+  for j in range(i,i+patternLen):
+    if text[j] != pattern[patternIndex]:
+      isValid = False
+    patternIndex+=1
+  if(isValid):
+    indexes.append(i)  
+
+print(indexes)
